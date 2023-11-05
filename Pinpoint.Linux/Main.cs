af79e55790ffc2b8fae67ab739cce4fd314d6e81
Nicholas <nicho.tjornelund@gmail.com>
2023-11-05 12:29:23 +0100

    linux first blood

---
+++ Pinpoint.Linux/Main.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Pinpoint.Core;
+using Pinpoint.Core.Results;
+using Pinpoint.Linux;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pinpoint.Plugin.Linux
+{
+    public class Main
+    {
+        public static void Main(string[] args)
+        {
+            var conf = new ConfigurationBuilder().AddJsonFile("file.json").Build;
+
+            var pipe = new PipeReader();
+            pipe.Listen();
+                 
+        }
+    }
+}
---
+++ Pinpoint.Linux/PipeReader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pinpoint.Linux
+{
+    public class PipeReader
+    {
+
+        public void Listen()
+        {
+
+            string pipeName = "df";
+
+            try
+            {
+                using var pipeClient = new NamedPipeClientStream(".", pipeName, PipeDirection.InOut);
+                pipeClient.Connect();
+
+                using var writer = new StreamWriter(pipeClient);
+                using var reader = new StreamReader(pipeClient);
+                while (true)
+                {
+                    string request = "Hello, Server!";
+                    Console.WriteLine($"Sending request: {request}");
+                    writer.WriteLine(request);
+                    writer.Flush();
+
+                    // Read the response from the server
+                    string response = reader.ReadLine();
+                    Console.WriteLine($"Received response: {response}");
+                }
+
+                
+
+
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
